fe31442ead1419aeb94fa10527f2054b86aa4b4e
Slavoljub Popovic <slavo@slavo.io>
2024-06-28 17:18:16 -0400

    udate

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -1,13 +1,21 @@
 import "./Hero.styles.css"
-
+import { useState } from "react";
 
 
 export const Hero = ({title, text}: any) => {
+    const [ score, setScore ] = useState( 0 );
+
+    const handleCount = () => setScore( score + 1 )
+
+    const handleReset = () => setScore( 0 )
     
     return (
         <section className="heroStyles">
             <h1>{title}</h1>
-            <p>{text}</p>
+            <p>{ text }</p>
+            <p>{ score }</p>
+            <button onClick={handleCount}>Count</button>
+            <button onClick={handleReset}>Reset</button>
         </section>
     );
 }
